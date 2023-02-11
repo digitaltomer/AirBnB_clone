0cf5e58ab3f964a699c8bf8e2c70045f125ba706
digitaltomer <tomamadin@gmail.com>
2023-02-11 13:52:32 -0800

    state models

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates a State User class"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """This Class is for managing state objects"""
+
+    name = ""
